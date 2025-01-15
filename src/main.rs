395f7c2a7f8cdc7c8f5f84149caf8d46a0437e02
samkahunga <samkahunga65@gmail.com>
2025-01-15 12:25:38 +0300

    figured out how to use folders.. further

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ mod controller {
     pub mod trial;
 }
 use actix_web::{web, App, HttpServer};
+use controller::trial;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("I am running at 127.0.0.1:8080");
     HttpServer::new(|| {
         App::new()
-            .service(controller::trial::hello)
-            .service(controller::trial::echo)
-            .route("/hey", web::get().to(controller::trial::manual_hello))
+            .service(trial::hello)
+            .service(trial::echo)
+            .route("/hey", web::get().to(trial::manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
